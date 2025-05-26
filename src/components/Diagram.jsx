d0e4b85ef0dd63d05fc212803e5bd4d558a67634
Nuno Cortez <nucortez1@gmail.com>
2025-05-26 18:43:07 +0100

    first commit

--- src/components/Diagram.jsx
+++ src/components/Diagram.jsx
@@ -1,6 +1,17 @@
 import Chord from "./Chord";
-
-
+import Dash from './src/assets/SVGs/Dash.svg';
+import Trigram from './src/assets/SVGs/Trigram.svg';
+import TrigramHorizontal from './src/assets/SVGs/TrigramHorizontal.svg';
+import ThreeVertical from './src/assets/SVGs/3vertical.svg';
+import FourVertical from './src/assets/SVGs/4vertical.svg';
+import FiveVertical from './src/assets/SVGs/5vertical.svg';
+import SixVertical from './src/assets/SVGs/6vertical.svg';
+import SevenVertical from './src/assets/SVGs/7vertical.svg';
+import ThreeHorizontal from './src/assets/SVGs/3horizontal.svg';
+import FourHorizontal from './src/assets/SVGs/4horizontal.svg';
+import FiveHorizontal from './src/assets/SVGs/5horizontal.svg';
+import SixHorizontal from './src/assets/SVGs/6horizontal.svg';
+import SevenHorizontal from './src/assets/SVGs/7horizontal.svg';
 
 
 
@@ -8,26 +19,26 @@ const Diagram = () => {
     return (
         <div className="diagram">
             <div className="vertical"> 
-                <Chord svg="/src/components/SVGs/Dash.svg" className="one" />
-                <Chord svg="/src/components/SVGs/Trigram.svg" className="two" />
-                <Chord svg="/src/components/SVGs/3vertical.svg" className="three" />
-                <Chord svg="/src/components/SVGs/4vertical.svg" className="four" />
-                <Chord svg="/src/components/SVGs/5vertical.svg" className="five" />
-                <Chord svg="/src/components/SVGs/6vertical.svg" className="six" />
-                <Chord svg="/src/components/SVGs/7vertical.svg" className="seven" />
-                <Chord svg="/src/components/SVGs/Trigram.svg" className="eight" />
-                <Chord svg="/src/components/SVGs/Dash.svg" className="nine" />
+                <Chord svg={Dash} className="one" />
+                <Chord svg={Trigram} className="two" />
+                <Chord svg={ThreeVertical} className="three" />
+                <Chord svg={FourVertical} className="four" />
+                <Chord svg={FiveVertical} className="five" />
+                <Chord svg={SixVertical} className="six" />
+                <Chord svg={SevenVertical} className="seven" />
+                <Chord svg={Trigram} className="eight" />
+                <Chord svg={Dash} className="nine" />
             </div>
             <div className="horizontal">
-                <Chord svg="/src/components/SVGs/Dash.svg" className="ten" />
-                <Chord svg="/src/components/SVGs/TrigramHorizontal.svg" className="eleven" />
-                <Chord svg="/src/components/SVGs/3horizontal.svg" className="twelve" />
-                <Chord svg="/src/components/SVGs/4horizontal.svg" className="thirteen" />
-                <Chord svg="/src/components/SVGs/5horizontal.svg" className="fourteen" />
-                <Chord svg="/src/components/SVGs/6horizontal.svg" className="fifteen" />
-                <Chord svg="/src/components/SVGs/7horizontal.svg" className="sixteen" />
-                <Chord svg="/src/components/SVGs/TrigramHorizontal.svg" className="seventeen" />
-                <Chord svg="/src/components/SVGs/Dash.svg" className="eighteen" />
+                <Chord svg={Dash} className="ten" />
+                <Chord svg={TrigramHorizontal} className="eleven" />
+                <Chord svg={ThreeHorizontal} className="twelve" />
+                <Chord svg={FourHorizontal} className="thirteen" />
+                <Chord svg={FiveHorizontal} className="fourteen" />
+                <Chord svg={SixHorizontal} className="fifteen" />
+                <Chord svg={SevenHorizontal} className="sixteen" />
+                <Chord svg={TrigramHorizontal} className="seventeen" />
+                <Chord svg={Dash} className="eighteen" />
             </div>
             <div className="squared"></div>
            
