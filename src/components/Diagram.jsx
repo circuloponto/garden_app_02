70190ffde5f53c217a7c30026dac05246ef7aa33
Nuno Cortez <nucortez1@gmail.com>
2025-05-26 18:48:05 +0100

    first commit

--- src/components/Diagram.jsx
+++ src/components/Diagram.jsx
@@ -1,18 +1,17 @@
 import Chord from "./Chord";
-import Dash from './src/assets/SVGs/Dash.svg';
-import Trigram from './src/assets/SVGs/Trigram.svg';
-import TrigramHorizontal from './src/assets/SVGs/TrigramHorizontal.svg';
-import ThreeVertical from './src/assets/SVGs/3vertical.svg';
-import FourVertical from './src/assets/SVGs/4vertical.svg';
-import FiveVertical from './src/assets/SVGs/5vertical.svg';
-import SixVertical from './src/assets/SVGs/6vertical.svg';
-import SevenVertical from './src/assets/SVGs/7vertical.svg';
-import ThreeHorizontal from './src/assets/SVGs/3horizontal.svg';
-import FourHorizontal from './src/assets/SVGs/4horizontal.svg';
-import FiveHorizontal from './src/assets/SVGs/5horizontal.svg';
-import SixHorizontal from './src/assets/SVGs/6horizontal.svg';
-import SevenHorizontal from './src/assets/SVGs/7horizontal.svg';
-
+import Dash from '../assets/SVGs/Dash.svg';
+import Trigram from '../assets/SVGs/Trigram.svg';
+import TrigramHorizontal from '../assets/SVGs/TrigramHorizontal.svg';
+import ThreeVertical from '../assets/SVGs/3vertical.svg';
+import FourVertical from '../assets/SVGs/4vertical.svg';
+import FiveVertical from '../assets/SVGs/5vertical.svg';
+import SixVertical from '../assets/SVGs/6vertical.svg';
+import SevenVertical from '../assets/SVGs/7vertical.svg';
+import ThreeHorizontal from '../assets/SVGs/3horizontal.svg';
+import FourHorizontal from '../assets/SVGs/4horizontal.svg';
+import FiveHorizontal from '../assets/SVGs/5horizontal.svg';
+import SixHorizontal from '../assets/SVGs/6horizontal.svg';
+import SevenHorizontal from '../assets/SVGs/7horizontal.svg';
 
 
 const Diagram = () => {
